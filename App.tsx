c997d076ff9e99c76ec6f6fdf087b5fd296c0447
Ido Lago <idolago94@gmail.com>
2023-08-19 18:50:09 +0300

    init app + navigation

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={[s.container, backgroundStyle]}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <View style={s.content}>
+          <Text>AT Prices</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center' 
+  }
+})
